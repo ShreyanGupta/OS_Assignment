e78082873059f5a27db8da70dea2890eb928344f
Shreyan <shreyan.gupta96@gmail.com>
2017-01-23 23:07:17 +0530

    Assignment 1.4

---
+++ shell.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "util/config.h"
+#include "util/debug.h"
+
+struct shellstate_t{
+    char curr_cmd[1<<9];
+    int curr_pos;
+    bool execute;
+    int num_key;
+};
+
+struct renderstate_t{
+};
+
+void shell_init(shellstate_t& state);
+void shell_update(uint8_t scankey, shellstate_t& stateinout);
+void shell_step(shellstate_t& stateinout);
+void shell_render(const shellstate_t& shell, renderstate_t& render);
+
+bool render_eq(const renderstate_t& a, const renderstate_t& b);
+void render(const renderstate_t& state, int w, int h, addr_t display_base);
+
