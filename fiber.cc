d39d466f00d4926f2dab9734d545d1a9c113d410
Shreyan <shreyan.gupta96@gmail.com>
2017-03-08 18:55:20 +0530

    debug karna hai

--- fiber.cc
+++ fiber.cc
@@ -1,19 +1,24 @@
 #include "labs/fiber.h"
 
-void fiber_facto(addr_t &main_stack, addr_t &f_stack, int xx, bool &running, int &answer){
-    int x=xx;
+void fiber_facto(addr_t *pmain_stack, addr_t *pf_stack, int *xx, bool *run, int *ans){
+    addr_t& main_stack = *pmain_stack; // boilerplate: to ease the transition from existing code
+    addr_t& f_stack    = *pf_stack;
+    int x = *xx;
+    int &answer = *ans;
+    bool &running = *run;
     answer = 1;
-    for(int i=0; i<=x; ++i){
+    for(int i=1; i<=x; ++i){
         answer *= i;
+        hoh_debug("yo peeps, i = " << i);
         stack_saverestore(f_stack, main_stack);
     }
-    running = false;
-
-    // while(true){
-    //     done = true;
-    //     ret = temp;
-    //     stack_saverestore(f_stack, main_stack);
-    // }
+    // running = false;
+    // stack_saverestore(f_stack, main_stack);
+    while(true){
+        running = false;
+        // ret = temp;
+        stack_saverestore(f_stack, main_stack);
+    }
 
 }
 
@@ -22,14 +27,15 @@ void shell_step_fiber(shellstate_t& shellstate, addr_t& main_stack, addr_t& f_st
     if(f.started){
         f.started = false;
         f.running = true;
-        // stackinit
-        // fiber_facto()
-        stack_init5(f_stack, f_array, f_arraysize, &fiber_facto, 
-            main_stack, f_stack, f.x, f.running, f.answer);
+        hoh_debug("f started is true! \n");
+        stack_init5(f_stack, &f_array, f_arraysize, &fiber_facto, 
+            &main_stack, &f_stack, &f.x, &f.running, &f.answer);
     }
     if(f.running){
+        hoh_debug("f running! \n");
         stack_saverestore(main_stack, f_stack);
         if(!f.running){
+            hoh_debug("f ended! \n");
             shellstate.insert_answer_fiber(f.answer);
         }
     }
